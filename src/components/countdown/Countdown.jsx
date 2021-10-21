811f1c2d257c8669f155d4083fb11f8c06eacf41
Soe Arkar Htet <developer.soearkarhtet@gmail.com>
2021-10-21 22:00:30 +0630

    refactor: readable to some functions

--- src/components/countdown/Countdown.jsx
+++ src/components/countdown/Countdown.jsx
@@ -1,12 +1,34 @@
 import React, { Component } from 'react';
 import './Countdown.scss';
 
+const calculateTime = (endDate) => {
+  const now = new Date().getTime();
+  const timeDistance = endDate - now;
+  
+  let days = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
+  let hours = Math.floor((timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  let minutes = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
+  let seconds = Math.floor((timeDistance % (1000 * 60)) / 1000);
+
+  days = days < 10 ? `0${days}` : days;
+  hours = hours < 10 ? `0${hours}` : hours;
+  minutes = minutes < 10 ? `0${minutes}` : minutes;
+  seconds = seconds < 10 ? `0${seconds}` : seconds;
+
+  return {
+    timeDistance,
+    days,
+    hours,
+    minutes,
+    seconds
+  }
+}
 export default class Countdown extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      // endDate: new Date("Jan 01, 2022 00:00:00").getTime(),
-      endDate: new Date("Oct 21, 2021 21:25:00").getTime(),
+      endDate: new Date("Jan 01, 2022 00:00:00").getTime(),
+      // endDate: new Date("Oct 21, 2021 21:57:00").getTime(),
       distance: true,
       days: `00`,
       hours: `00`,
@@ -17,7 +39,7 @@ export default class Countdown extends Component {
 
   componentDidMount() {
     this.timer = setInterval(
-      () => this.calculateTime(),
+      () => this.tick(),
       1000
     );
   }
@@ -26,26 +48,15 @@ export default class Countdown extends Component {
     clearInterval(this.timer);
   }
 
-  calculateTime() {
-    const now = new Date().getTime();
-    const timeDistance = this.state.endDate - now;
-    
-    let d = Math.floor(timeDistance / (1000 * 60 * 60 * 24));
-    let hr = Math.floor((timeDistance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let min = Math.floor((timeDistance % (1000 * 60 * 60)) / (1000 * 60));
-    let sec = Math.floor((timeDistance % (1000 * 60)) / 1000);
-
-    d = d < 10 ? `0${d}` : d;
-    hr = hr < 10 ? `0${hr}` : hr;
-    min = min < 10 ? `0${min}` : min;
-    sec = sec < 10 ? `0${sec}` : sec;
+  tick() {
+    const {timeDistance, days, hours, minutes, seconds} = calculateTime(this.state.endDate);
 
     if (timeDistance > 0) {
       this.setState({
-        days: d,
-        hours: hr,
-        minutes: min,
-        seconds: sec,
+        days,
+        hours,
+        minutes,
+        seconds,
       });
     } else {
       this.setState({
